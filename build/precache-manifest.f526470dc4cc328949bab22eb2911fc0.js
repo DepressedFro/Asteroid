0e96da9c023dc06270bee848adc82735554be216
DepressedFro <the.guya@yahoo.com>
2019-08-14 23:48:30 -0500

    Small updates

---
+++ build/precache-manifest.f526470dc4cc328949bab22eb2911fc0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8efbbd543bda1f759c99f34f20d1a4",
+    "url": "/DepressedFro/Asteroid/index.html"
+  },
+  {
+    "revision": "c5741a78b271c0a95e41",
+    "url": "/DepressedFro/Asteroid/static/css/main.8d1433f8.chunk.css"
+  },
+  {
+    "revision": "c5741a78b271c0a95e41",
+    "url": "/DepressedFro/Asteroid/static/js/main.efaadea1.chunk.js"
+  },
+  {
+    "revision": "da496c9278e42d841e20",
+    "url": "/DepressedFro/Asteroid/static/js/runtime~main.6fd84a11.js"
+  }
+]);
